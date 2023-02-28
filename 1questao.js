f93a92733064d2f24b6948bb4cb111bc5815c01b
Ana Flavia de Sousa Monteiro <107011686+anafsmonteiro@users.noreply.github.com>
2023-02-28 11:29:38 -0300

    Update 1questao.js

--- 1questao.js
+++ 1questao.js
@@ -1,5 +1,5 @@
 let num = 0;
-let par = [], imp = [];
+let par = [], imp = []; 
 
 var prompt = require('prompt-sync')({ sigint: true });
 
@@ -7,15 +7,17 @@ for (let i = 1; i <= 30; i++) {
     num = parseInt(prompt("Digite um número:"));
     if (num % 2 == 0) {
         par.push(num);
-        if (par.length == 5) {
-            console.log(par);
-            par.length = 0
+        if (par.length == 6) {
+            par.splice(0,1)
+            console.log(`PAR: ${par}`);
         }
     } else {
         imp.push(num);
-        if (imp.length == 5) {
-            console.log(imp);
-            imp.length = 0
+        if (imp.length == 6) {
+            imp.splice(0,1)
+            console.log(`IMPARr: ${imp}`);
         }
     }
 }
+console.log(par);
+console.log(imp);
